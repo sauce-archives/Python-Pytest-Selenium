060779d6fddd14f9688fbe474046fb18db827cb9
Mehmet Gerceker <mehmetg@msn.com>
2016-02-12 02:43:18 -0800

    Refactored the tests slightly. Testing guinea pig now. Added browsers on desktop, iOS and android.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Python-Pytest-Selenium
+## Python-Pytest-Appium-Android
 
 This code is provided on an "AS-IS” basis without warranty of any kind, either express or implied, including without limitation any implied warranties of condition, uninterrupted use, merchantability, fitness for a particular purpose, or non-infringement. Your tests and testing environments may require you to modify this framework. Issues regarding this framework should be submitted through GitHub. For questions regarding Sauce Labs integration, please see the Sauce Labs documentation at https://wiki.saucelabs.com/. This framework is not maintained by Sauce Labs Support.
 
@@ -10,7 +10,7 @@ This code is provided on an "AS-IS” basis without warranty of any kind, either
     ```
     $ brew install python
     ```
-    * Install [pip](https://pip.pypa.io/en/stable/installing/) to install packages
+    * Install [pip](https://pip.pypa.io/en/stable/installing/) for package installation
 
 2. Sauce Credentials
     * In the terminal export your Sauce Labs Credentials as environmental variables:
@@ -18,17 +18,20 @@ This code is provided on an "AS-IS” basis without warranty of any kind, either
     $ export SAUCE_USERNAME=<your Sauce Labs username>
 	$ export SAUCE_ACCESS_KEY=<your Sauce Labs access key>
     ```
-3. Project Dependencies
-	* Install packages (Use sudo if necessary)
-	```
-	$ pip install selenium sauceclient pytest pytest-xdist 
-	```
-### Running Tests
+3. Project
+	* The recommended way to run your tests would be in [virtualenv](https://virtualenv.readthedocs.org/en/latest/). It will isolate the build from other setups you may have running and ensure that the tests run with the specified versions of the modules specified in the requirements.txt file.
+	```$ pip install virtualenv```
+	* Create a virtual environment in your project folder the environment name is arbitrary.
+	```$ virtualenv venv```
+	* Activate the environment:
+	```$ source venv/bin/activate```
+	* Install the required packages:
+	```$ pip install -r requirements.txt```
+
+### Running Tests:  -n option designates number of parallel tests and -s to disable output capture.
 
 Tests in Parallel:
-	```
-	$ make
-	```
+```$ py.test -s -n 10 tests```
 
 [Sauce Labs Dashboard](https://saucelabs.com/beta/dashboard/)
 
@@ -40,7 +43,7 @@ There may be additional latency when using a remote webdriver to run tests on Sa
 ### Resources
 ##### [Sauce Labs Documentation](https://wiki.saucelabs.com/)
 
-##### [SeleniumHQ Documentation](http://www.seleniumhq.org/docs/)
+##### [Selenium Documentation](http://www.seleniumhq.org/docs/)
 
 ##### [Python Documentation](https://docs.python.org/2.7/)
 
@@ -48,3 +51,10 @@ There may be additional latency when using a remote webdriver to run tests on Sa
 
 ##### [Stack Overflow](http://stackoverflow.com/)
 * A great resource to search for issues not explicitly covered by documentation.
+
+### Kown Issues:
+* Test output will be captured in .testlog files as the pytest-xdist plugin has issues with not capturing stdout and stderr. You can use the following commands to output session id's for CI integration and clean up.
+```
+$ cat *.testlog
+$ rm -rf *.testlog
+```
--- tests/base_test.py
+++ tests/base_test.py
@@ -5,19 +5,46 @@ import new
 from selenium import webdriver
 from sauceclient import SauceClient
 
-username = os.environ['SAUCE_USERNAME']
-access_key = os.environ['SAUCE_ACCESS_KEY']
-
 browsers = [{
+    'browserName': 'android',
+    'platform': 'Linux',
+    'version': '4.3',
+    'deviceName': 'Android Emulator',
+    'deviceOrientation': 'portrait',
+    'name': 'Python Selenium Android 4.3 example'
+}, {
+    'browserName': 'android',
+    'platform': 'Linux',
+    'version': '5.1',
+    'deviceName': 'Android Emulator',
+    'deviceOrientation': 'portrait',
+    'name': 'Python Selenium Android 5.1 example'
+}, {
+    'browserName': 'iPhone',
+    'platform': 'Mac 10.10',
+    'version': '9.2',
+    'deviceName': 'iPhone 6',
+    'deviceOrientation': 'portrait',
+    'name': 'Python Selenium iOS 9.2 example'
+}, {
+    'browserName': 'iPhone',
+    'platform': 'Mac 10.10',
+    'platformVersion': '8.4',
+    'deviceName': 'iPhone 6',
+    'deviceOrientation': 'portrait',
+    'name': 'Python Selenium iPhone 6 iOS 8.4 example'
+}, {
     "platform": "Windows 10",
     "browserName": "internet explorer",
     "version": "11"
 }, {
-    "platform": "OS X 10.10",
+    "platform": "OS X 10.11",
     "browserName": "safari",
-    "version": "8"
+    "version": "9"
 }]
 
+
+# This decorator is required to iterate over browsers
 def on_platforms(platforms):
     def decorator(base_class):
         module = sys.modules[base_class.__module__].__dict__
@@ -26,29 +53,56 @@ def on_platforms(platforms):
             d['desired_capabilities'] = platform
             name = "%s_%s" % (base_class.__name__, i + 1)
             module[name] = new.classobj(name, (base_class,), d)
+
     return decorator
 
-def log_to_file(data):
-    if os.environ.has_key("LOG_OUTPUT"):
-        with open("result_log.txt", "a") as f:
-            f.write(data + "\n")
 
 class BaseTest(unittest.TestCase):
+    username = None
+    access_key = None
+    selenium_port = None
+    selenium_host = None
+    upload = True
+    tunnel_id = None
+    build_tag = None
 
     # setUp runs before each test case
     def setUp(self):
         self.desired_capabilities['name'] = self.id()
+
+        if BaseTest.tunnel_id:
+            self.desired_capabilities['tunnel-identifier'] = BaseTest.tunnel_id
+        if BaseTest.build_tag:
+            self.desired_capabilities['build'] = BaseTest.build_tag
+
         self.driver = webdriver.Remote(
-           command_executor="http://%s:%s@ondemand.saucelabs.com:80/wd/hub" % (username, access_key),
-           desired_capabilities=self.desired_capabilities)
+                command_executor="http://%s:%s@%s:%s/wd/hub" %
+                                 (BaseTest.username,
+                                  BaseTest.access_key,
+                                  BaseTest.selenium_host,
+                                  BaseTest.selenium_port),
+                desired_capabilities=self.desired_capabilities)
 
     # tearDown runs after each test case
     def tearDown(self):
         self.driver.quit()
-        session_id = self.driver.session_id
-        job_name = self.id()
-        sauce_client = SauceClient(username, access_key)
+        sauce_client = SauceClient(BaseTest.username, BaseTest.access_key)
         status = (sys.exc_info() == (None, None, None))
-        sauce_client.jobs.update_job(session_id, passed=status)
-        output = "SauceOnDemandSessionID=%s job-name=%s" % (session_id, job_name)
-        log_to_file(output)
+        sauce_client.jobs.update_job(self.driver.session_id, passed=status)
+        test_name = "%s_%s" % (type(self).__name__, self.__name__)
+        with(open(test_name + '.testlog', 'w')) as outfile:
+            outfile.write("SauceOnDemandSessionID=%s job-name=%s\n" % (self.driver.session_id, test_name))
+
+    @classmethod
+    def setup_class(cls):
+        cls.build_tag = os.environ.get('BUILD_TAG', None)
+        cls.tunnel_id = os.environ.get('TUNNEL_IDENTIFIER', None)
+        cls.username = os.environ.get('SAUCE_USERNAME', None)
+        cls.access_key = os.environ.get('SAUCE_ACCESS_KEY', None)
+
+        cls.selenium_port = os.environ.get("SELENIUM_PORT", None)
+        if cls.selenium_port:
+            cls.selenium_host = "localhost"
+        else:
+            cls.selenium_host = "ondemand.saucelabs.com"
+            cls.selenium_port = "80"

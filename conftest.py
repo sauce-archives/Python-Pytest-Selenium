d0d3bbe780dd621419cd8d81ac8d385c6b214029
joshin4colours <joshua.m.grant@gmail.com>
2019-05-28 11:06:18 -0400

    Updates to modernize test example (#16)
    * some people use VS Code
    
    * fixup to safari versions
    
    * updated platform config
    
    * replaced older tests with new sauce demo app tests
    
    * updated reporting results to sauce
    
    * updated some dependencies
    
    * updated locators
    
    * assertion update
    
    * small update based on Titus
    
    * some changes for python 2.7 compatibility

--- conftest.py
+++ conftest.py
@@ -5,18 +5,48 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.remote.remote_connection import RemoteConnection
 
+import urllib3
+urllib3.disable_warnings()
 
-@pytest.fixture(scope='function')
-def driver(request):
-    # if the assignment below does not make sense to you please read up on object assignments.
-    # The point is to make a copy and not mess with the original test spec.
-    desired_caps = {}
-    
-    browser = {
+browsers = [
+    {
+        "seleniumVersion": '3.4.0',
+        "platform": "Windows 10",
+        "browserName": "MicrosoftEdge",
+        "version": "14.14393"
+    }, {
+        "seleniumVersion": '3.4.0',
         "platform": "Windows 10",
         "browserName": "firefox",
-        "version": "49.0"
-    }
+        "version": "60.0"
+    }, {
+        "seleniumVersion": '3.4.0',
+        "platform": "Windows 7",
+        "browserName": "internet explorer",
+        "version": "11.0"
+    }, {
+        "seleniumVersion": '3.4.0',
+        "platform": "OS X 10.12",
+        "browserName": "safari",
+        "version": "11.0"
+    }, {
+        "seleniumVersion": '3.4.0',
+        "platform": "OS X 10.11",
+        "browserName": "chrome",
+        "version": "69.0",
+        "extendedDebugging": True
+    }]
+
+def pytest_generate_tests(metafunc):
+    if 'driver' in metafunc.fixturenames:
+        metafunc.parametrize('browser_config',
+                             browsers,
+                             ids=_generate_param_ids('broswerConfig', browsers),
+                             scope='function')
+
+
+def _generate_param_ids(name, values):
+    return [("<%s:%s>" % (name, value)).replace('.', '_') for value in values]
 
     desired_caps.update(browser)
     test_name = request.node.name
@@ -34,7 +64,8 @@ def driver(request):
     executor = RemoteConnection(selenium_endpoint, resolve_ip=False)
     browser = webdriver.Remote(
         command_executor=executor,
-        desired_capabilities=desired_caps
+        desired_capabilities=desired_caps, 
+        keep_alive=True
     )
     yield browser
 
@@ -47,14 +78,18 @@ def driver(request):
     else:
         raise WebDriverException("Never created!")
 
-    def fin():
-        browser.execute_script("sauce:job-result={}".format(str(not request.node.rep_call.failed).lower()))
-        browser.quit()
-    request.addfinalizer(fin)
+    yield browser
+    # Teardown starts here
+    # report results
+    # use the test result to send the pass/fail status to Sauce Labs
+    sauce_result = "failed" if request.node.rep_call.failed else "passed"
+    browser.execute_script("sauce:job-result={}".format(sauce_result))
+    browser.quit()
 
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
 def pytest_runtest_makereport(item, call):
-    # this sets the result as a test attribute for SauceLabs reporting.
+    # this sets the result as a test attribute for Sauce Labs reporting.
     # execute all other hooks to obtain the report object
     outcome = yield
     rep = outcome.get_result()
@@ -62,3 +97,4 @@ def pytest_runtest_makereport(item, call):
     # set an report attribute for each phase of a call, which can
     # be "setup", "call", "teardown"
     setattr(item, "rep_" + rep.when, rep)
+
